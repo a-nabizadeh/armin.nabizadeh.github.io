e351fed0d5c9a81ecc82671f8e18a7fa727aed0e
a-nabizadeh <92112224+a-nabizadeh@users.noreply.github.com>
2023-07-10 00:01:28 +0300

    V1

---
+++ script.js
@@ -0,0 +1,20 @@
+// Get references to the sections
+const researchSection = document.getElementById('research');
+const publicationsSection = document.getElementById('publications');
+const cvSection = document.getElementById('cv');
+
+// Add event listeners to the sections
+researchSection.addEventListener('click', () => {
+  // Action to perform when Research section is clicked
+  console.log('Research section clicked');
+});
+
+publicationsSection.addEventListener('click', () => {
+  // Action to perform when Publications section is clicked
+  console.log('Publications section clicked');
+});
+
+cvSection.addEventListener('click', () => {
+  // Action to perform when CV section is clicked
+  console.log('CV section clicked');
+});
