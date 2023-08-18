5daf68b6ab4a1b97a1b7fa4fa3b2ac0128a04dc7
a-nabizadeh <92112224+a-nabizadeh@users.noreply.github.com>
2023-08-18 12:56:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
-
 async function getpapers() {
   try {
-      const response = await fetch('https://serpapi.com/search.json?engine=google_scholar&q=author:armin%20nabizadeh&api_key=56e6ee3f4a7f850c0076ad10c14bdcc31e0371c82ecd04e7a5630e377a9a5754');
+      const response = await fetch('https://ui.adsabs.harvard.edu/search/q=%20%20author%3A%22%5Eleroy%2Ca%22&sort=date%20desc%2C%20bibcode%20desc&p_=0');
+      log.console(response);
       const data = await response.json();
 
       const resultsDiv = document.getElementById('results');
@@ -28,17 +27,4 @@ async function getpapers() {
   }
 }
 
-const SerpApi = require('google-search-results-nodejs');
-const search = new SerpApi.GoogleSearch("56e6ee3f4a7f850c0076ad10c14bdcc31e0371c82ecd04e7a5630e377a9a5754");
-
-const params = {
-  engine: "google_scholar",
-  q: "author:armin nabizadeh"
-};
-
-const callback = function(data) {
-  console.log(data["organic_results"]);
-};
-
-// Show result as JSON
-search.json(params, callback);
+getpapers();
