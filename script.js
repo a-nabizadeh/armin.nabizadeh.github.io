526e7d6111dfd8ff6470233c39b1d24c2cc6a1b1
a-nabizadeh <92112224+a-nabizadeh@users.noreply.github.com>
2023-08-18 12:08:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-// Get references to the sections
-const researchSection = document.getElementById('research');
-const publicationsSection = document.getElementById('publications');
-const cvSection = document.getElementById('cv');
+async function getpapers() {
+  try {
+      const response = await fetch('https://serpapi.com/search.json?engine=google_scholar&q=author:armin%20nabizadeh&api_key=56e6ee3f4a7f850c0076ad10c14bdcc31e0371c82ecd04e7a5630e377a9a5754');
+      const data = await response.json();
 
-// Add event listeners to the sections
-researchSection.addEventListener('click', () => {
-  // Action to perform when Research section is clicked
-  console.log('Research section clicked');
-});
+      const resultsDiv = document.getElementById('results');
 
-publicationsSection.addEventListener('click', () => {
-  // Action to perform when Publications section is clicked
-  console.log('Publications section clicked');
-});
+      for (const result of data.organic_results) {
+          const title = result.title;
+          const authors = result.publication_info.authors.map(author => author.name).join(', ');
+          const citations = result.inline_links.cited_by.total;
 
-cvSection.addEventListener('click', () => {
-  // Action to perform when CV section is clicked
-  console.log('CV section clicked');
-});
+          const resultHTML = `
+              <div>
+                  <h2>${title}</h2>
+                  <p>Authors: ${authors}</p>
+                  <p>Citations: ${citations}</p>
+                  <hr>
+              </div>
+          `;
+
+          resultsDiv.innerHTML += resultHTML;
+      }
+  } catch (error) {
+      console.error("Error fetching the data:", error);
+  }
+}
+getpapers();
